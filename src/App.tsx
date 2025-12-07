73a86167c9ac33e113acf491f0af37c78545a683
Raihan-17 <aburaihanrantu009@gmail.com>
2025-12-08 02:41:05 +0600

    applied bg and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import Navbar from './sections/Navbar';
+import Wrapper from './sections/Wrapper';
+import Footer from './sections/Footer';
+import Background from './components/Background';
+import "./scss/index.scss";
 
 const App = () => {
   return (
     <div className='main-container'>
-      no
+      <Background />
+      <div className="app">
+        <Navbar />
+        <Wrapper />
+        <Footer />
+      </div>
     </div>
   );
 };
